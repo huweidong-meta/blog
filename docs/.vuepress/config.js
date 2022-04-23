f1d621c7e48395d8c883303c36cacd3c7240f4db
hello <hwd888@aliyun.com>
2022-04-23 19:37:54 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+const dayjs = require('dayjs')
+require('dayjs/locale/zh-cn')
+const dayjs_plugin_localizedFormat = require('dayjs/plugin/localizedFormat')
+module.exports = {
+    title:"hwd",
+    plugins: {
+            '@vuepress/last-updated': {
+                transformer: (timestamp, lang) => {
+                dayjs.extend(dayjs_plugin_localizedFormat)
+                dayjs.locale('zh-cn')
+                return dayjs(timestamp).format('LLLL')
+            }
+        },
+    },
+    themeConfig: {
+        logo: 'logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: '#' },
+            { text: 'about', link: '/about/' },
+        ],
+        sidebar: [
+            '/',
+            '/page-a',
+            ['/page-b', 'Explicit link text']
+        ],
+        lastUpdated: '最后更新时间', // string | boolean
+    }
+}
